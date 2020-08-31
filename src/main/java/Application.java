16d3bb2d3b7ca901aea33cf45749b422c3552806
Jose Diaz <jadiaz@farmaciasperuanas.pe>
2020-08-31 18:14:15 -0500

    remove spring

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,7 +2,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Savepoint;
 import java.sql.Statement;
 import java.time.LocalDateTime;
 
@@ -15,84 +14,103 @@ import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
 public class Application {
 
 	public static void main(String[] args) throws SQLException {
+		// transaction isolation levels
+
 		DataSource ds = createDataSource();
-		Connection connection = ds.getConnection();
-		try(connection) {
 
-			connection.setAutoCommit(false);
+		int senderId = -1;
+		int receiverId = -1;
 
-			int senderId = createUser(connection);
-			int receiverId = createUser(connection);
+		Connection connection1 = ds.getConnection();
+		try (connection1) {
+			connection1.setAutoCommit(false);
 
-			Savepoint savepoint = connection.setSavepoint();
+			senderId = createUser(connection1);
+			receiverId = createUser(connection1);
 
-			int transferId = sendMoney(connection, senderId, receiverId, 50);
-
-			if (transferId<0) connection.rollback(savepoint);
-
-			connection.commit();
-		}catch (SQLException e){
-			connection.rollback();
+			connection1.commit();
+		} catch (SQLException e) {
+			connection1.rollback();
 		}
 
-	}
 
-	private static int sendMoney(Connection connection, int senderId, int receiverId, int amount) throws SQLException {
-		try (PreparedStatement stmt = connection.prepareStatement(
-				"update users set balance= (balance - ?) where id = ?")) {
-			stmt.setInt(1, amount);
-			stmt.setInt(2, senderId);
-			// implicit: open transaction
-			stmt.executeUpdate();
-			// implicit: commit/close transaction
+		Connection connection2 = ds.getConnection();
+
+		try (connection2) {
+			connection2.setAutoCommit(false);
+
+			Connection connection3 = ds.getConnection();
+			try (connection3) {
+				connection3.setAutoCommit(false);
+
+				Integer connection3BalanceBefore = getBalance(connection3, senderId);
+				System.out.println("connection3BalanceBefore = "
+						+ connection3BalanceBefore);
+
+				try (PreparedStatement stmt = connection2.prepareStatement(
+						"update users set balance = (balance - ?) where id = ?")) {
+					stmt.setInt(1, 99);
+					stmt.setInt(2, senderId);
+					stmt.executeUpdate();
+				}
+				// TODO update into receiver's balance
+				// TODO insert into transactions table
+
+				Integer connection3BalanceAfter = getBalance(connection3, senderId);
+				System.out.println("connection3BalanceAfter = "
+						+ connection3BalanceAfter);
+				connection3.commit();
+			}
+			connection2.commit();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			connection2.rollback();
 		}
-
-		try (PreparedStatement stmt = connection.prepareStatement(
-				"update users set balance= (balance + ?) where id = ?")) {
-			stmt.setInt(1, amount);
-			stmt.setInt(2, receiverId);
-			// implicit: open transaction
-			stmt.executeUpdate();
-			// implicit: commit/close transaction
-		}
-
-		try (PreparedStatement stmt = connection.prepareStatement(
-				"insert into transfers (sender_id, receiver_id, amount) "
-						+ "values (?,?,?)", Statement.RETURN_GENERATED_KEYS)) {
-			stmt.setInt(1, senderId);
-			stmt.setInt(2, receiverId);
-			stmt.setInt(3, amount);
-			// implicit: open transaction
-			stmt.executeUpdate();
-			// implicit: commit/close transaction
-			ResultSet rs = stmt.getGeneratedKeys();
-			rs.next();
-			return rs.getInt(1);
-		}
-
 	}
 
 	private static int createUser(Connection connection) throws SQLException {
-		try (PreparedStatement stmt = connection.prepareStatement("insert into users (first_name, last_name, registration_date) "
-				+ "values(?,?,?)", Statement.RETURN_GENERATED_KEYS)) {
-			stmt.setString(1, "[first name]");
-			stmt.setString(2, "[last name]");
+		try (PreparedStatement stmt = connection.prepareStatement("insert into " +
+						"users (first_name, last_name, registration_date) values " +
+						"(?,?,?)"
+				, Statement.RETURN_GENERATED_KEYS)) {
+			stmt.setString(1, "[Some FirstName]");
+			stmt.setString(2, "[Some LastName]");
 			stmt.setObject(3, LocalDateTime.now());
 			stmt.executeUpdate();
 
-			ResultSet rs = stmt.getGeneratedKeys();
-			rs.next();
-			return rs.getInt(1);
+			final ResultSet keysResultSet = stmt.getGeneratedKeys();
+			keysResultSet.next();
+			return keysResultSet.getInt(1);
 		}
-
 	}
+
+
+	private static Integer getBalance(Connection connection, int userId) throws SQLException {
+		Integer balance = null;
+
+		try (PreparedStatement stmt = connection.prepareStatement(
+				"select balance " +
+						"from users where id = ?")) {
+
+			stmt.setInt(1, userId);
+
+			ResultSet resultSet = stmt.executeQuery();
+			while (resultSet.next()) {
+				balance = resultSet.getInt("balance");
+				break;
+			}
+		}
+		return balance;
+	}
+
 
 
 	public static DataSource createDataSource(){
 
 		//docker run -p 3306:3306 --name mysql8 -e MYSQL_ROOT_PASSWORD=joedayz -e MYSQL_DATABASE=mydb -d mysql:8
 		HikariDataSource hikariDs = new HikariDataSource();
-		hikariDs.setJdbcUrl("jdbc:h2:~/mydatabase;INIT=RUNSCRIPT FROM 'classpath:schema.sql'");
+		hikariDs.setJdbcUrl("jdbc:h2:~/mydatabase");
 		hikariDs.setUsername("sa");
 		hikariDs.setPassword("password");
 
